6d58bf9a34acf2020b3d7c1705dc7661ef9ee109
Dylan Timmons <dylanstimmons@icloud.com>
2023-04-11 13:56:37 -0600

    reassign string variables

--- variables.rb
+++ variables.rb
@@ -1,11 +1,25 @@
 name = "Dylan"
+name # "Dylan"
+name = "Dylan Timmons"
+
 email = "Dylanstimmons@icloud.com"
+email # "Dylanstimmons@icloud.com"
+email = "Dylantimmons1@gmail.com"
+
 age = 31
+
 shoe_size = 10
+
 debt_amount = 4000.00
+
 net_worth = 1.00
+
 needs_coffee = True
+
 needs_redbull = True
 
+puts(name+shoe_size)
+puts(name+net_worth)
+puts(name+email)
 
 
